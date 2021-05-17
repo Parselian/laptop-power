8be5d16a68da85c5c3560b28f132695152aaa7c1
Parselian <parselian.study@yandex.ru>
2021-05-17 16:31:37 +0300

    finished design of features and pricelist sections and adapted them to all devices

--- src/index.php
+++ src/index.php
@@ -41,6 +41,7 @@
 		</li>
 	</ul>
 </div>
+
 <section class="promo">
 	<header class="header">
 		<div class="container header__wrap">
@@ -81,6 +82,167 @@
 			<button class="button button_callback promo__button">Срочный ремонт</button>
 			<button class="button button_accent-transparent promo__button">Получить скидку</button>
 		</div>
+	</div>
+</section>
+
+<section class="features">
+	<div class="container features__wrap">
+		<div class="features__feature">
+			<svg class="features__feature-icon">
+				<use xlink:href="./assets/stack/sprite.svg#guarantee"></use>
+			</svg>
+			<div class="features__feature-title">Пожизненная <span class="line-break">гарантия</span></div>
+		</div>
+		<div class="features__feature">
+			<svg class="features__feature-icon">
+				<use xlink:href="./assets/stack/sprite.svg#health-report"></use>
+			</svg>
+			<div class="features__feature-title">Бесплатная <span class="line-break">диагностика</span> </div>
+		</div>
+		<div class="features__feature">
+			<svg class="features__feature-icon">
+				<use xlink:href="./assets/stack/sprite.svg#gear"></use>
+			</svg>
+			<div class="features__feature-title">Ремонт <span class="line-break">любой сложности</span> </div>
+		</div>
+	</div>
+</section>
+
+<section class="prices">
+	<div class="container prices__wrap">
+		<h2 class="section__title section__title_alt prices__title">Услуги сервисного центра <?= $company_name?></h2>
+
+		<div class="prices__devices">
+			<button data-device="laptops" class="prices__devices-button">Ноутбуки</button>
+			<button data-device="monoblocks" class="prices__devices-button prices__devices-button_active">Моноблоки</button>
+			<button data-device="computers" class="prices__devices-button">Компьютеры</button>
+			<button data-device="servers" class="prices__devices-button">Серверы</button>
+		</div>
+		<div class="prices__models">
+			<button data-model="Acer" class="prices__models-button">Acer</button>
+			<button data-model="Asus" class="prices__models-button prices__models-button_active">Asus</button>
+			<button data-model="Apple" class="prices__models-button">Apple</button>
+			<button data-model="Samsung" class="prices__models-button">Samsung</button>
+			<button data-model="Huawei" class="prices__models-button">Huawei</button>
+			<button data-model="Xiaomi" class="prices__models-button">Xiaomi</button>
+			<button data-model="Honor" class="prices__models-button">Honor</button>
+		</div>
+
+		<div class="prices__common-blocks">
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/display-replacement.webp" type="image/webp">
+					<img src="./assets/images/display-replacement.jpg" alt="замена дисплея на ноутбуке" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Замена дисплея</span>
+					<span class="prices__common-block-price">1 000 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/laptop-clean.webp" type="image/webp">
+					<img src="./assets/images/laptop-clean.jpg" alt="Чистка ноутбука" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Чистка ноутбука</span>
+					<span class="prices__common-block-price line-break">390 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/keyboard-repair.webp" type="image/webp">
+					<img src="./assets/images/keyboard-repair.jpg" alt="Ремонт клавиатуры" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Ремонт клавиатуры</span>
+					<span class="prices__common-block-price line-break">250 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/cooling-system-repair.webp" type="image/webp">
+					<img src="./assets/images/cooling-system-repair.jpg" alt="Ремонт системы охлаждения" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Ремонт системы охлаждения</span>
+					<span class="prices__common-block-price line-break">390 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/power-chains-repair.webp" type="image/webp">
+					<img src="./assets/images/power-chains-repair.jpg" alt="Ремонт цепей питания" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Ремонт цепей питания</span>
+					<span class="prices__common-block-price line-break">590 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/motherboard-repair.webp" type="image/webp">
+					<img src="./assets/images/motherboard-repair.jpg" alt="Ремонт цепей питания" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Ремонт материнской платы</span>
+					<span class="prices__common-block-price line-break">690 ₽</span>
+				</div>
+			</div>
+
+			<div class="prices__common-block">
+				<picture>
+					<source srcset="./assets/images/webp/software-install.webp" type="image/webp">
+					<img src="./assets/images/software-install.jpg" alt="Установка программ" class="prices__common-block-img">
+				</picture>
+				<div class="prices__common-block-info">
+					<span class="prices__common-block-name">Установка и настройка ПО</span>
+					<span class="prices__common-block-price line-break">250 ₽</span>
+				</div>
+			</div>
+		</div>
+
+		<ul class="prices__pricelist">
+			<li class="prices__pricelist-item">
+				<span class="prices__pricelist-item-name">Ремонт материнской платы ноутбука</span>
+				<div class="prices__pricelist-item-wrap">
+					<div class="prices__pricelist-item-label">+ скидка 20%</div>
+					<button class="prices__pricelist-item-button">Вызвать мастера</button>
+					<div class="prices__pricelist-item-price">1 990 ₽</div>
+				</div>
+			</li>
+			<li class="prices__pricelist-item">
+				<span class="prices__pricelist-item-name">Ремонт материнской платы ноутбука</span>
+				<div class="prices__pricelist-item-wrap">
+					<div class="prices__pricelist-item-label">+ скидка 20%</div>
+					<button class="prices__pricelist-item-button">Вызвать мастера</button>
+					<div class="prices__pricelist-item-price">1 990 ₽</div>
+				</div>
+			</li>
+			<li class="prices__pricelist-item">
+				<span class="prices__pricelist-item-name">Ремонт материнской платы ноутбука</span>
+				<div class="prices__pricelist-item-wrap">
+					<div class="prices__pricelist-item-label">+ скидка 20%</div>
+					<button class="prices__pricelist-item-button">Вызвать мастера</button>
+					<div class="prices__pricelist-item-price">1 990 ₽</div>
+				</div>
+			</li>
+			<li class="prices__pricelist-item">
+				<span class="prices__pricelist-item-name">Ремонт материнской платы ноутбука</span>
+				<div class="prices__pricelist-item-wrap">
+					<div class="prices__pricelist-item-label">+ скидка 20%</div>
+					<button class="prices__pricelist-item-button">Вызвать мастера</button>
+					<div class="prices__pricelist-item-price">1 990 ₽</div>
+				</div>
+			</li>
+		</ul>
+
+		<button class="button button_accent prices__pricelist-unroll">Показать всё</button>
 	</div>
 </section>
 </body>
